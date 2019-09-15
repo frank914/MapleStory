2574605909b0c1f43cbcfc77c1e215549370ec3c
frank914 <50728808+frank914@users.noreply.github.com>
2019-09-15 23:41:31 +0800

    Add files via upload

---
+++ npc/初戀谷的升級NPC.js
@@ -0,0 +1,109 @@
+﻿var rewardItem = -1 ,reqItemQty = -1, reqEqp = -1, SuccessRand = -1, ExplosionRand = -1;
+var status = 0;
+var reqItem = 4001523;
+var reqPoint =10000; //升階需求楓點
+var items = [
+[1132243,2000,-1,100,0,false], 
+[1122264,2000,-1,100,0,false],
+[1032220,3000,-1,100,0,false],
+[1113072,3000,-1,100,0,false],
+
+[1132244,1500,1132243,70,0],
+[1122265,1500,1122264,70,0],
+[1032221,1500,1032220,70,0],
+[1113073,1500,1113072,70,0],
+
+[1132245,2000,1132244,50,20],
+[1122266,2000,1122265,50,20],
+[1032222,2000,1032221,50,20],
+[1113074,2000,1113073,50,20],
+
+[1132246,3000,1132245,30,30],
+[1122267,3000,1122266,30,30],
+[1032223,3000,1032222,30,30],
+[1113075,3000,1113074,30,30]
+
+] ; // 獲得物, 需要楓葉, 需求物, 成功率 破壞率
+function start() {
+    action(1, 0, 0);
+}
+
+function action(mode, type, selection) {
+    if (mode == 1) {
+        status++
+    } else if (mode == 0) {
+        status--;
+    } else {
+        cm.dispose();
+        return;
+    }
+    if (status <= 0) {
+	cm.sendOk("謝謝光臨，歡迎下次再來~");
+	cm.dispose();
+    }
+    if (status == 1) {
+	var msg = "";
+	msg += "您好我是8月活動NPC，活動物品全域掉落哦~，我們提供的兌換有\r\n";
+	for ( i = 0; i < items.length ; i++ ) {
+		rewardItem = items[i][0];
+		reqItemQty = items[i][1];
+		reqEqp = items[i][2]; 
+		SuccessRand = items[i][3];
+		ExplosionRand = items[i][4];
+		msg += "\r\n#L"+i+"#用"+reqItemQty+"個#i"+reqItem+":#兌換#i" + rewardItem  +":#成功率:"+SuccessRand+" 破壞率:"+ExplosionRand+"\r\n" ;
+		if ( reqEqp >= 0 )
+			msg += "\t額外需求裝備:#i"+reqEqp+":##t"+reqEqp+":#\r\n\t並且需要:"+reqPoint+"楓葉點數\r\n"
+	}
+	cm.sendSimple(msg);
+    }else if ( status == 2 ) {
+	sel = selection;
+	rewardItem = items[sel][0];
+	reqItemQty = items[sel][1];
+	reqEqp = items[sel][2]; 
+	SuccessRand = items[sel][3];
+	ExplosionRand = items[sel][4];
+	if ( !cm.hasSpace(rewardItem,1) ) {
+		cm.sendNext("哀呀，看來你的裝備欄沒有空間欸~");
+		cm.dispose();
+		return;
+	} else if ( !cm.haveItem(reqItem,reqItemQty) ) {
+		cm.sendOk("所有的#i"+reqItem+":#不足到"+reqItemQty+"個");
+		cm.dispose();
+		return;
+	} else if ( reqEqp > 0 && !cm.haveItem(reqEqp,1) ) {
+		cm.sendOk("你好像沒有#i"+reqEqp+":##t"+reqEqp+":#欸，檢查看看!");
+		cm.dispose();
+		return;
+	} else if (cm.getPlayer().getCSPoints(2) < reqPoint && reqEqp > 0) {
+		cm.sendOk("點數不足需要"+reqPoint);
+		cm.dispose();
+		return;
+	}
+		msg = "\r\n請問你是否要用"+reqItemQty+"個#i"+reqItem+":#兌換#i" + rewardItem  +":#\r\n成功率:"+SuccessRand+" 破壞率:"+ExplosionRand+"\r\n" ;
+		if ( reqEqp >= 0 )
+			msg += "額外需求裝備:#i"+reqEqp+":##t"+reqEqp+":#\r\n並且需要:"+reqPoint+"楓葉點數\r\n"
+	cm.sendYesNo(msg);
+    } else if ( status == 3 ) {
+	var Rand = getRandomInt(1,100) ;
+	var Rand_ExPlosion = getRandomInt(1,100) ;
+	cm.gainItem(reqItem,-reqItemQty);
+	if ( reqEqp > 0 ){
+		cm.getPlayer().modifyCSPoints(2, -reqPoint, true);
+		cm.gainItem(reqEqp, -1);
+	}
+	if ( Rand <= SuccessRand ){
+			cm.gainItem(rewardItem, 1);
+			cm.sendOk("合成成功，檢查看看背包吧");
+	} else {
+		if ( Rand_ExPlosion <= ExplosionRand && reqEqp > 0){
+			cm.gainItem(reqEqp, -1);
+			cm.sendOk("合成失敗，而且連#i"+reqEqp+":##t"+reqEqp+":#也跟著爆炸了!!");
+		} else 
+			cm.sendOk("哀呀，合成失敗了!!");
+    }
+	cm.dispose();
+	}
+}
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
---
+++ npc/飄飄谷妞妞.js
@@ -0,0 +1,240 @@
+﻿var status = -1;
+var Name = Array(
+"黑桃A","黑桃2","黑桃3","黑桃4","黑桃5","黑桃6","黑桃7","黑桃8","黑桃9","黑桃10","黑桃J","黑桃Q","黑桃K",
+"紅心A","紅心2","紅心3","紅心4","紅心5","紅心6","紅心7","紅心8","紅心9","紅心10","紅心J","紅心Q","紅心K",
+"方塊A","方塊2","方塊3","方塊4","方塊5","方塊6","方塊7","方塊8","方塊9","方塊10","方塊J","方塊Q","方塊K",
+"梅花A","梅花2","梅花3","梅花4","梅花5","梅花6","梅花7","梅花8","梅花9","梅花10","梅花J","梅花Q","梅花K");
+var Point = Array(
+1,2,3,4,5,6,7,8,9,10,100,100,100,
+1,2,3,4,5,6,7,8,9,10,100,100,100,
+1,2,3,4,5,6,7,8,9,10,100,100,100,
+1,2,3,4,5,6,7,8,9,10,100,100,100);
+var MesoBase = 100; //楓幣基礎值
+var MesoMin = 100; //楓幣min
+var MesoMax = 1000000; //楓幣max 
+var Point1_Base = 100; //楓點基礎值
+var Point1_Min = 100; //楓點min
+var Point1_Max = 1000000; //楓點max 
+var Point2_Base = 100; //GASH基礎值
+var Point2_Min = 100; //GASHmin
+var Point2_Max = 1000000; //GASHmax 
+var typed ; // 選擇類別
+var typedName = ["楓幣","楓點","GASH"];
+var Consume ; //消費值
+var Player =["無妞",0,0];
+var PlayerH =Array();
+var PlayerP =Array();
+var Computer =["無妞",0,0];
+var ComputerH =Array();
+var ComputerP =Array();
+var Odds = 100 ; //賠率 % 數
+var ran ;
+var PointVector = [
+["無妞",0,0], 
+["一點",1,1], 
+["二點",2,1], 
+["三點",3,1], 
+["四點",4,1], 
+["五點",5,1], 
+["六點",6,1], 
+["七點",7,2], 
+["八點",8,2], 
+["九點",9,2], 
+["妞妞",10,3], 
+["黑龍",11,5]
+]; // 牌名,順位,倍率
+
+function action(mode, type, selection) {
+    if (mode == 1) {
+        status++;
+    } else {
+        if (status >= 1) {
+            status = 1;
+            //return;
+        } else {
+            cm.dispose();
+            return;
+        }
+        status--;
+    }
+    if (status == 0) {
+        var text = "你好，歡迎來到#rXX谷#b#e妞妞系統#n#k\r\n";
+        text += "#L0#使用楓幣賭博#l\r\n";
+        text += "#L1#使用楓點賭博#l\r\n";
+        text += "#L2#使用GASH賭博#l\r\n";
+        cm.sendSimple(text);
+    } else if (status == 1) {
+        typed = selection;
+        var text = "請輸入你要投注的金額：";
+        if (selection == 0) {
+            cm.sendGetNumber(text, MesoBase, MesoMin, MesoMax);
+        } else if (selection == 1) {
+            cm.sendGetNumber(text, Point1_Base, Point1_Min, Point1_Max);
+        } else if (selection == 2) {
+            cm.sendGetNumber(text, Point2_Base, Point2_Min, Point2_Max);
+        }
+    } else if (status == 2) {
+		Consume = selection; 
+		if ( typed == 0 ){
+			if ( cm.getPlayer().getMeso() < Consume*PointVector[11][2] ){
+				cm.sendOk("您的金額不足以負擔最慘的狀況欸");
+				cm.dispose();
+				return;
+			}else
+				cm.gainMeso(-Consume);
+		} else if ( typed == 1 ) {
+			if ( cm.getPlayer().getCSPoints(2) < Consume*PointVector[11][2] ){
+				cm.sendOk("您的金額不足以負擔最慘的狀況欸");
+				cm.dispose();
+				return;
+			}else
+				cm.getPlayer().modifyCSPoints(-Consume, 2, true);
+		} else if ( typed == 2 ) {
+			if ( cm.getPlayer().getCSPoints(1) < Consume*PointVector[11][2] ){
+				cm.sendOk("您的金額不足以負擔最慘的狀況欸");
+				cm.dispose();
+				return;
+			}else
+				cm.getPlayer().modifyCSPoints(-Consume, 1, true);
+		}
+		text  = "#b帥哥/美女\r\n#k";
+		text += "您下注的額度是"+Consume+typedName[typed]+"\r\n\r\n";
+		text += "#r#e點選下一步開自己牌";
+		Licensing(); // 發牌
+        cm.sendNext(text);
+    } else if (status == 3) {
+		Judgebrand(Player,PlayerP);
+		text  = "#b帥哥/美女\r\n#k";
+		text += "您下注的額度是"+Consume+typedName[typed]+"\r\n\r\n";
+		text += "你目前手上的牌"+PlayerH[0]+"."+PlayerH[1]+"."+PlayerH[2]+"."+PlayerH[3]+"."+PlayerH[4]+":#b"+Player[0]+"\r\n";
+		text += "#r#e點選下一步開莊家牌";
+        cm.sendNext(text);
+    } else if (status == 4) {
+		Judgebrand(Computer,ComputerP);
+		text  = "#b帥哥/美女\r\n#k";
+		text += "您下注的額度是"+Consume+typedName[typed]+"\r\n\r\n";
+		text += "你目前手上的牌"+PlayerH[0]+"."+PlayerH[1]+"."+PlayerH[2]+"."+PlayerH[3]+"."+PlayerH[4]+":#b"+Player[0]+"\r\n";
+		text += "#k莊家目前手上的牌"+ComputerH[0]+"."+ComputerH[1]+"."+ComputerH[2]+"."+ComputerH[3]+"."+ComputerH[4]+":#b"+Computer[0]+"\r\n";
+		text += "#r#e點選下一步比較點數";
+        cm.sendNext(text);
+    } else if (status == 5) {
+		if (CompareSize( Player[1], Computer[1] )) {
+			text = "你目前手上的牌"+PlayerH[0]+"."+PlayerH[1]+"."+PlayerH[2]+"."+PlayerH[3]+"."+PlayerH[4]+":#b"+Player[0]+"\r\n";
+			text += "#k比莊家目前手上的牌"+ComputerH[0]+"."+ComputerH[1]+"."+ComputerH[2]+"."+ComputerH[3]+"."+ComputerH[4]+":#b"+Computer[0]+"\r\n";
+			text += "#k還大，#r#e 賠率:"+Player[2]+"\r\n";
+			text += "點選下一步領獎";
+			cm.sendNext(text);
+		} else {
+			if ( typed == 0 ){
+				cm.gainMeso(-Consume*(Computer[2]-1));
+			} else if ( typed == 1 ) {
+				cm.getPlayer().modifyCSPoints(-Consume*Computer[2], 2, true);
+			} else if ( typed == 2 ) {
+				cm.getPlayer().modifyCSPoints(-Consume*Computer[2], 1, true);
+			}
+			text = "你目前手上的牌"+PlayerH[0]+"."+PlayerH[1]+"."+PlayerH[2]+"."+PlayerH[3]+"."+PlayerH[4]+":#b"+Player[0]+"\r\n";
+			text += "#k比莊家目前手上的牌"+ComputerH[0]+"."+ComputerH[1]+"."+ComputerH[2]+"."+ComputerH[3]+"."+ComputerH[4]+":#b"+Computer[0]+"\r\n";
+			text += "#k還小，#r#e 賠率:"+Computer[2]+"\r\n";
+			text += "已接受賠率懲罰，點選下一步離開。";
+			cm.sendNext(text);
+		}
+		
+    } else if (status == 6) {
+		if (CompareSize( Player[1], Computer[1] )) {
+			if ( typed == 0 ){
+				cm.gainMeso(Consume*Player[2]);
+			} else if ( typed == 1 ) {
+				cm.getPlayer().modifyCSPoints(Consume*Player[2], 2, true);
+			} else if ( typed == 2 ) {
+				cm.getPlayer().modifyCSPoints(Consume*Player[2], 1, true);
+			}
+
+		}
+			cm.sendOk("謝謝光臨，歡迎下次再來玩!");
+		cm.dispose();
+	}
+}
+
+function Licensing(){
+	var finishOne = false;
+	for ( var i = 0 ; i < 5 ; i++ ){
+	  while ( !finishOne ){
+		ran = parseInt(Math.random()*Name.length);
+		finishOne = exambrand();
+	  }
+	  PlayerH[i] = Name[ran];
+	  PlayerP[i] = Point[ran];
+	finishOne = false;
+	}
+	for ( var i = 0 ; i < 5 ; i++ ){
+	  while ( !finishOne ){
+		ran = parseInt(Math.random()*Name.length);
+		finishOne = exambrand();
+	  }
+	  ComputerH[i] = Name[ran];
+	  ComputerP[i] = Point[ran];
+	finishOne = false;
+	}
+}
+
+function exambrand () {
+	for(i=0;i<Player.length;i++){
+		if(Player[i]==Name[ran])
+			return false;
+	}
+	for(i=0;i<Computer.length;i++){
+		if(Computer[i]==Name[ran])
+			return false;
+	}
+	return true;
+}
+
+function Judgebrand(name,nameP){ // 1.2.3 1.2.4 1.2.5 2.3.4 2.3.5 3.4.5 五搭都要檢查紀錄大小
+	if ( ( nameP[0]+nameP[1]+nameP[2]+nameP[3]+nameP[4] ) / 500 == 1 ) { // 黑龍
+		name[0] = PointVector[UsePoint][0];
+		name[1] = PointVector[UsePoint][1];
+		name[2] = PointVector[UsePoint][2];
+	}
+	if ( ( nameP[0] + nameP[1] + nameP[2] ) % 10 == 0 ) 
+		Judgebrand2(name,nameP,3,4);
+	if ( ( nameP[0] + nameP[1] + nameP[3] ) % 10 == 0 ) 
+		Judgebrand2(name,nameP,2,4);
+	if ( ( nameP[0] + nameP[1] + nameP[4] ) % 10 == 0 ) 
+		Judgebrand2(name,nameP,2,3);
+	if ( ( nameP[0] + nameP[2] + nameP[3] ) % 10 == 0 ) 
+		Judgebrand2(name,nameP,1,4);
+	if ( ( nameP[0] + nameP[2] + nameP[4] ) % 10 == 0 ) 
+		Judgebrand2(name,nameP,1,3);
+	if ( ( nameP[0] + nameP[3] + nameP[4] ) % 10 == 0 ) 
+		Judgebrand2(name,nameP,1,2);
+	if ( ( nameP[1] + nameP[2] + nameP[3] ) % 10 == 0 ) 
+		Judgebrand2(name,nameP,0,4);
+	if ( ( nameP[1] + nameP[2] + nameP[4] ) % 10 == 0 ) 
+		Judgebrand2(name,nameP,0,3);
+	if ( ( nameP[1] + nameP[3] + nameP[4] ) % 10 == 0 ) 
+		Judgebrand2(name,nameP,0,2);
+	if ( ( nameP[2] + nameP[3] + nameP[4] ) % 10 == 0 ) 
+		Judgebrand2(name,nameP,0,1);
+	
+
+}
+function Judgebrand2(name,nameP,first,second){
+  	UsePoint = nameP[first] + nameP[second];
+	UsePoint = UsePoint % 10;
+	if ( UsePoint == 0 )
+		UsePoint += 10;
+	if ( name[1] <= PointVector[UsePoint][1] ){
+		name[0] = PointVector[UsePoint][0];
+		name[1] = PointVector[UsePoint][1];
+		name[2] = PointVector[UsePoint][2];
+	}
+	
+	
+}
+
+function CompareSize( PlayerSize, ComputerSize ) { // 玩家大電腦 RETURN TRUE
+	if ( Player > ComputerSize )
+		return true;
+	else
+		return false;
+}
